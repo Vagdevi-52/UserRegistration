6699b33ee9db68c27d03a321acb969706141717e
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-10 08:35:22 -0700

    Added: Program To Enter First Name

--- src/userregistration/UserRegistration.java
+++ src/userregistration/UserRegistration.java
@@ -1,11 +1,36 @@
 package userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 class UserRegistration
 {
-	public static void main(String[] args) 
-	{
-		System.out.println("Welcome to user registration page");
 
-	}
+    Scanner scanner = new Scanner(System.in);
+    public static final String NAMEPATTERN = "[A-Z][A-Za-z]{2,}";
 
+    public static void main(String[] args)
+    {
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.readInput();
+    }
+    private void readInput() //Method for reading an input.
+    {
+        System.out.println("Enter first name ");
+        String firstName = scanner.next();
+        validateInput(NAMEPATTERN,firstName);
+    }
+
+    private void validateInput(String pattern, String input)
+    {
+
+        if(Pattern.matches(pattern, input))
+        {
+            System.out.println("Valid");
+        }
+        else
+        {
+            System.out.println("Invalid");
+        }
+    }
 }
