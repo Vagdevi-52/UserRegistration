dfd01238824c819340aeb42e986d6ab8df19779f
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-10 08:41:42 -0700

    Added: Program To Enter Last Name

--- src/userregistration/UserRegistration.java
+++ src/userregistration/UserRegistration.java
@@ -3,10 +3,11 @@ package userregistration;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-class UserRegistration
+class UserRegistration 
 {
 
     Scanner scanner = new Scanner(System.in);
+
     public static final String NAMEPATTERN = "[A-Z][A-Za-z]{2,}";
 
     public static void main(String[] args)
@@ -14,11 +15,11 @@ class UserRegistration
         UserRegistration userRegistration = new UserRegistration();
         userRegistration.readInput();
     }
-    private void readInput() //Method for reading an input.
+    private void readInput()
     {
-        System.out.println("Enter first name ");
-        String firstName = scanner.next();
-        validateInput(NAMEPATTERN,firstName);
+        System.out.println("Enter last name ");
+        String lastName = scanner.next();
+        validateInput(NAMEPATTERN,lastName);
     }
 
     private void validateInput(String pattern, String input)
