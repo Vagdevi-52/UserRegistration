9dcac2ddb21ececf0bfdddf48af3d77514dcaeef
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-11 03:13:02 -0700

    Added: Program Should Have Exactly One Special Charcter

--- src/userregistration/UserRegistration.java
+++ src/userregistration/UserRegistration.java
@@ -3,28 +3,25 @@ package userregistration;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-public class UserRegistration 
+public class UserRegistration
 {
 
     Scanner scanner = new Scanner(System.in);
 
-    public static final String PASSWORDPATTERN = "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9@$!%*#?&]{6,}$";
-
-    public static void main(String[] args) 
+    public static final String PASSWORDPATTERN = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*?&]{5,}$";
+    public static void main(String[] args)
     {
         UserRegistration userRegistration = new UserRegistration();
         userRegistration.readInput();
     }
-    private void readInput() //Read An Input From The User.
+    private void readInput() //Read an input from the user.
     {
-        System.out.println("Enter the password ");
+        System.out.println("Enter password ");
         String Password = scanner.next();
         validateInput(PASSWORDPATTERN,Password);
     }
-
     private void validateInput(String pattern, String input)
     {
-
         if(Pattern.matches(pattern, input))
         {
             System.out.println("Valid");
@@ -35,4 +32,3 @@ public class UserRegistration
         }
     }
 }
-
