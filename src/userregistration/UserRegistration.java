0daaeb9e31570629aed835596a66b601c34c7b0b
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-11 03:02:59 -0700

    Added: Program Should Have Atleast One Numeric Number

--- src/userregistration/UserRegistration.java
+++ src/userregistration/UserRegistration.java
@@ -3,21 +3,21 @@ package userregistration;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
-class UserRegistration
+public class UserRegistration 
 {
 
     Scanner scanner = new Scanner(System.in);
 
-    public static final String PASSWORDPATTERN = "^(?=.*[A-Z])[0-9a-zA-Z@$!%*#?&]{8,}$";
+    public static final String PASSWORDPATTERN = "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9@$!%*#?&]{6,}$";
 
-    public static void main(String[] args)
+    public static void main(String[] args) 
     {
         UserRegistration userRegistration = new UserRegistration();
         userRegistration.readInput();
     }
-    private void readInput() //Read an input from the user.
+    private void readInput() //Read An Input From The User.
     {
-        System.out.println("Enter the Password ");
+        System.out.println("Enter the password ");
         String Password = scanner.next();
         validateInput(PASSWORDPATTERN,Password);
     }
@@ -35,3 +35,4 @@ class UserRegistration
         }
     }
 }
+
